785f1c1f954de2c02e6675fddf2bc4ef2587629e
Murasya <vrydy.hppnss@icloud.com>
2019-07-10 13:12:34 +0900

    JFileChooserによりファイルを選択できるようにした

--- src/File.java
+++ src/File.java
@@ -4,40 +4,54 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Vector;
 
+import javax.swing.JFileChooser;
+
 public class File {
 	Vector<MyDrawing> v;
 	Mediator mediator;
+	JFileChooser fc;
+	int returnVal;
 
 	public File(Mediator mediator) {
 		this.mediator = mediator;
 		v = mediator.drawings;
+		fc = new JFileChooser();
 	}
 
     // File入力
 	public Vector<MyDrawing> fileInput() {
-		System.out.println("read file.txt");
+		System.out.println("Read File");
 		try {
-			FileInputStream fin = new FileInputStream("file.txt");
-	        ObjectInputStream in = new ObjectInputStream(fin);
+			returnVal = fc.showOpenDialog(null);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				java.io.File file = fc.getSelectedFile();
+				FileInputStream fin = new FileInputStream(file);
+		        ObjectInputStream in = new ObjectInputStream(fin);
 
-	        v = (Vector<MyDrawing>)in.readObject();
-	        fin.close();
-	        mediator.drawings = v;
-	        mediator.repaint();
+		        v = (Vector<MyDrawing>)in.readObject();
+		        fin.close();
+		        mediator.drawings = v;
+		        mediator.repaint();
+			}
 	    } catch (Exception ex) {
 	    }
 		return v;
 	}
     // File出力
 	public void fileOutput() {
+		System.out.println("Save File");
 	    try {
-	        FileOutputStream fout = new FileOutputStream("file.txt");
-	        ObjectOutputStream out = new ObjectOutputStream(fout);
+	    	returnVal = fc.showSaveDialog(null);
+	    	if (returnVal == JFileChooser.APPROVE_OPTION) {
+	    		java.io.File file = fc.getSelectedFile();
+		        FileOutputStream fout = new FileOutputStream(file);
+		        ObjectOutputStream out = new ObjectOutputStream(fout);
 
-	        out.writeObject(v);
-	        out.flush();
+		        out.writeObject(v);
+		        out.flush();
 
-	        fout.close();
+		        fout.close();
+	    	}
 	    } catch (Exception ex) {
 	    }
 	}
